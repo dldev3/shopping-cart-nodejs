686e20d18e4c8c8844f8f43d4934abddac12e0ae
d_liyanage <admliyanage@gmail.com>
2020-04-21 20:24:02 +0530

    user model created

---
+++ models/user.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("User", userSchema)
--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,7 @@ router.get('/', function(req,res,next){
     for(var i = 0; i< docs.length; i += chunkSize){
       productChunks.push(docs.slice(i, i + chunkSize));
     }
-
-    //console.log(productChunks);
-    
+    //console.log(productChunks);    
     res.render('shop/index', {
      productChunks
     });
